99b7104a4f6e3cfdc3fd6451a6d82fe79b183722
MartialProjects <omkarcbhoite.ocb@gmail.com>
2020-08-30 13:56:08 +0530

    Changes in salon

--- src/index.js
+++ src/index.js
@@ -11,9 +11,10 @@ const port = process.env.PORT;
 
 
 app.use(function (req, res, next) {
+    res.header("Content-Type", "application/json");
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
-    res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method, Access-Control-Request-Headers");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization");
     next()
 })
 //For maintainenece activity if wont site to down
